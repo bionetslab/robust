a740e99cc65a0e933f525415a416f4b025ace623
JudithBernett <bernett@in.tum.de>
2021-07-04 20:42:52 +0200

    some fixes for the robustness tests

--- pcst_approach/utils/ppi/__init__.py
+++ pcst_approach/utils/ppi/__init__.py
@@ -1,4 +1,5 @@
 from .ppi_instance import PpiInstance
 from .edge_weights import UnitEdgeWeight, CoVexEdgeWeight
 from .read_ppi import read_ppi
+from .read_ppi_shuffled import read_ppi_shuffled
 from .read_terminals import read_terminals
